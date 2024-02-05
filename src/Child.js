a984d4df58b9b75afe4c1cca44293a0f3f49e8a2
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-05 13:18:56 +0300

    completed assignment

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor ,color}) {
+  //console.log(onChangeColor);
+  function handlelick(){
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  
+  return <div onClick={handlelick} className="child" style={{ backgroundColor: color }} />;
 }
 
 export default Child;
